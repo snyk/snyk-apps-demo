28f3d6f04eed78a3cea45b6d32784a17af08ff2e
Love Bhardwaj <love.bhardwaj@snyk.io>
2021-09-16 19:36:00 +0530

    chore: add todos

--- src/lib/controllers/callback/callbackController.ts
+++ src/lib/controllers/callback/callbackController.ts
@@ -16,6 +16,25 @@ export class CallbackController implements Controller {
 
   private callback(req: Request, res: Response, next: NextFunction) {
     // Callback related verifications can be done here
+    console.log('Return params: ', req.query);
+    /**
+     * Example:
+     * {
+            code: '8ghYHKGfqIi1Gahgu5i8riOhHBtkJgyISLVS846Mbmc.WvBFMCLWOGp0hzjsd9xCG4clt9CMj1vd-RewRnxCqjY',
+            scope: '',
+            state: 'Qy8 QWoi4qOw4l5SE7aEgw=='
+        }
+     */
+    // TODO: call https://snyk.io/api/v3/apps/oauth2/token
+    // {
+    //     grant_type: authorization_code,
+    //     code: code,
+    //     redirect_uri: redirect URI - must match above,
+    //     client_id: clientId,
+    //     client_secret: clientSecret
+    //     }
+    // TODO: Fetch the access_token and refresh_token
+
     return res.render('callback');
   }
 }

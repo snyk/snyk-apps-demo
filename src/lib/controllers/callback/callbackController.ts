1f225713c4d3d1a285dcbbc3b98196c4f7fdb591
Darrell Mozingo <darrell@mozingo.net>
2021-09-17 22:38:17 -0400

    Use prod hostnames
    Also clean up authorize url building a bit.

--- src/lib/controllers/callback/callbackController.ts
+++ src/lib/controllers/callback/callbackController.ts
@@ -27,7 +27,7 @@ export class CallbackController implements Controller {
     try {
       const result = await axios({
         method: 'POST',
-        url: 'http://localhost:3846/oauth2/token',
+        url: 'https://api.snyk.io/v3/apps/oauth2/token',
         data: qs.stringify({
           grant_type: 'authorization_code',
           code,

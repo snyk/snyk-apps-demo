34ed7281cb8d9ffa1dab2e34301b2f226f32f469
Darrell Mozingo <darrell@mozingo.net>
2021-09-18 22:55:56 -0400

    Use proper db path for reading

--- src/lib/utils/db/readFromDb.ts
+++ src/lib/utils/db/readFromDb.ts
@@ -1,11 +1,9 @@
 import { Low, JSONFile } from 'lowdb';
-import { join } from 'path';
 import { AuthData } from '../../types';
 import { dbPath } from '../../../app';
 
 export async function readFromDb() {
-  const dbFile = join(__dirname, dbPath);
-  const adapter = new JSONFile<AuthData>(dbFile);
+  const adapter = new JSONFile<AuthData>(dbPath);
   const db = new Low<AuthData>(adapter);
   await db.read();
   return db.data;

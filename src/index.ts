775f60b6072f0896706ca48f864c7c3858d9d0de
Darrell Mozingo <darrell@mozingo.net>
2021-09-18 23:54:39 -0400

    Add simple (ie ugly) admin page
    Lists installs from the db, each person that authed the app for their
    org.

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,11 @@ import {
   SettingsController,
 } from './lib/controllers';
 import App from './app';
+import { AdminController } from './lib/controllers/admin/adminController';
 
 new App(
   [
+    new AdminController(),
     new HomepageController(),
     new CallbackController(),
     new ProjectsController(),

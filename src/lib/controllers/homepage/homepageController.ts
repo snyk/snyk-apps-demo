6b771459e408a5f149503ddb5193cc98b019644e
Love Bhardwaj <love.bhardwaj@snyk.io>
2021-09-20 23:34:42 +0530

    feat: add passport oauth2 strategy

--- src/lib/controllers/homepage/homepageController.ts
+++ src/lib/controllers/homepage/homepageController.ts
@@ -1,7 +1,11 @@
 import type { Controller } from '../../types';
+import { Envars } from '../../types';
 import type { Request, Response, NextFunction } from 'express';
 import { Router } from 'express';
 import { generateUrl } from '../../utils';
+import passport from 'passport';
+import OAuth2Strategy from 'passport-oauth2';
+import { APP_BASE } from '../../../app';
 
 export class HomepageController implements Controller {
   public path = '/';

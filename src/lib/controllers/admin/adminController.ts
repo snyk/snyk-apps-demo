3215834df29b7a967b1cdef4d2b4c309d4c6f40c
Love Bhardwaj <love.bhardwaj@snyk.io>
2021-09-24 22:51:34 +0530

    chore: code cleanup and jsdocs

--- src/lib/controllers/admin/adminController.ts
+++ src/lib/controllers/admin/adminController.ts
@@ -3,19 +3,36 @@ import type { NextFunction, Request, Response } from 'express';
 import { Router } from 'express';
 import { readFromDb } from '../../utils/db';
 
+/**
+ * The AdminController class for handling admin page
+ * and related requests. Every controller class
+ * implements the controller interface which
+ * has two members the path and the router.
+ */
 export class AdminController implements Controller {
+  // The base URL path for this controller
   public path: string = '/admin';
+  // Express router for this controller
   public router: Router = Router();
 
+  /**
+   * The constructor is used to initialize the
+   * routes for this controller
+   */
   constructor() {
     this.initRoutes();
   }
 
   private initRoutes() {
+    // Admin route to get to the app settings
     this.router.get(`${this.path}`, this.settings);
+    // Admin route to get to all the installs for the Snyk app
     this.router.get(`${this.path}/installs`, this.installs);
   }
 
+  /**
+   * @returns Renders the admin page with all installs from the database
+   */
   private async installs(req: Request, res: Response, next: NextFunction) {
     const db = await readFromDb();
     return res.render('admin-installs', {
@@ -23,7 +40,10 @@ export class AdminController implements Controller {
     });
   }
 
+  /**
+   * @returns Renders the settings page
+   */
   private async settings(req: Request, res: Response, next: NextFunction) {
-    return res.render('admin-settings', { loading: false });
+    return res.render('admin-settings');
   }
 }
--- src/lib/controllers/auth/authController.ts
+++ src/lib/controllers/auth/authController.ts
@@ -2,14 +2,31 @@ import { Controller } from '../../types';
 import { Router } from 'express';
 import passport from 'passport';
 
+/**
+ * The AuthController class for handling authentication
+ * of the app via Snyk Authorization flow. This is the
+ * third step of passport setup. Every controller class
+ * implements the controller interface which has two
+ * members the path and the router.
+ */
 export class AuthController implements Controller {
+  // The base URL path for this controller
   public path = '/auth';
+  // Express router for this controller
   public router = Router();
 
+  /**
+   * The constructor is used to initialize the
+   * routes for this controller
+   */
   constructor() {
     this.initRoutes();
   }
 
+  /**
+   * The /auth route is called to authenticate the Appp
+   * via Snyk using passportjs authenticate method
+   */
   private initRoutes() {
     this.router.get(`${this.path}`, passport.authenticate('oauth2'));
   }

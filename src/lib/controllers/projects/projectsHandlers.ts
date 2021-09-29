431272aede66da82ec355ef8b1220542d79eeec4
Love Bhardwaj <love.bhardwaj@snyk.io>
2021-09-29 23:56:11 +0530

    chore: update prettier and add format script

--- src/lib/controllers/projects/projectsHandlers.ts
+++ src/lib/controllers/projects/projectsHandlers.ts
@@ -12,32 +12,27 @@ import { AuthData, Envars } from '../../types';
  * @returns List of user project or an empty array
  */
 export async function getProjectsFromApi() {
-  try {
-    // Read data from DB
-    const db = await readFromDb();
-    const data = mostRecent(db.installs);
-    // If no data return empty array
-    if (!data) return [];
+  // Read data from DB
+  const db = await readFromDb();
+  const data = mostRecent(db.installs);
+  // If no data return empty array
+  if (!data) return [];
 
-    // Decrypt data(access token)
-    const eD = new EncryptDecrypt(process.env[Envars.EncryptionSecret] as string);
-    const access_token = eD.decryptString(data?.access_token);
-    const token_type = data?.token_type;
-    // Call Snyk API to get projects for the user
-    const result = await axios({
-      method: 'POST',
-      url: `${API_BASE}/v1/org/${data?.orgId}/projects`,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `${token_type} ${access_token}`,
-      },
-    });
-    // Return projects array or an empty array
-    return result.data.projects || [];
-  } catch (error) {
-    // Throws error if DB read or API call fails
-    throw error;
-  }
+  // Decrypt data(access token)
+  const eD = new EncryptDecrypt(process.env[Envars.EncryptionSecret] as string);
+  const access_token = eD.decryptString(data?.access_token);
+  const token_type = data?.token_type;
+  // Call Snyk API to get projects for the user
+  const result = await axios({
+    method: 'POST',
+    url: `${API_BASE}/v1/org/${data?.orgId}/projects`,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${token_type} ${access_token}`,
+    },
+  });
+  // Return projects array or an empty array
+  return result.data.projects || [];
 }
 
 /**

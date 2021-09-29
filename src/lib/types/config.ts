aa398288b7bf87f669e561a08d076fe008f89a1f
Darrell Mozingo <darrell@mozingo.net>
2021-09-29 17:05:09 -0400

    Remove unused config constants
    We're still pulling from .env for the time being.
    Can add them back when needed.

--- src/lib/types/config.ts
+++ src/lib/types/config.ts
@@ -1,9 +1,4 @@
 export const enum Config {
-  ClientID = 'client.id',
-  ClientSecret = 'client.secret',
-  EncryptionSecret = 'ecryptionSecret',
-  RedirectUri = 'redirectUri',
-  Scopes = 'scopes',
   ApiBase = 'api.base',
   AppBase = 'app.base',
   AuthURL = 'authorizationUrl',

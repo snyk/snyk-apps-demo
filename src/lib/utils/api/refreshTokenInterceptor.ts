c3366ba041cd439d08605da70d3302c25dc878ec
John Gresty <john.gresty@snyk.io>
2021-09-30 16:31:00 +0100

    chore: Simplify some verbose conventions
    - Remove a pointless try-catch block
    - Pass raw functions instead of wrapping them in lambdas

--- src/lib/utils/api/refreshTokenInterceptor.ts
+++ src/lib/utils/api/refreshTokenInterceptor.ts
@@ -15,40 +15,36 @@ import { updateDb } from '../db';
  * @returns Axios request interceptor
  */
 export async function refreshTokenInterceptor(request: AxiosRequestConfig) {
-  try {
-    // Read the latest data(auth token, refresh token and expiry)
-    const db = await readFromDb();
-    const data = mostRecent(db.installs);
-    // If no data then continue with the request
-    if (!data) return request;
-    // Data used to calculate the expiry
-    const expiresIn = data.expires_in;
-    const createdDate = data.date;
-    // Used npm library luxon to parse the date and calculate expiry
-    const parsedCreateDate = DateTime.fromISO(createdDate.toString());
-    const expirationDate = parsedCreateDate.plus({ seconds: expiresIn });
-    // Check if expired
-    if (expirationDate < DateTime.now()) {
-      // Create a instance for encryption and decryption
-      const eD = new EncryptDecrypt(process.env[Envars.EncryptionSecret] as string);
-      // Make request to refresh token
-      const { access_token, expires_in, refresh_token, scope, token_type } = await refreshAuthToken(
-        eD.decryptString(data.refresh_token),
-      );
-      // Update the acess and refresh token with the newly fetched access and refresh token
-      // along with the expiry and other required info
-      await updateDb(data, {
-        ...data,
-        access_token: eD.encryptString(access_token),
-        expires_in,
-        refresh_token: eD.encryptString(refresh_token),
-        token_type,
-        scope,
-        date: new Date(),
-      });
-    }
-  } catch (error) {
-    throw error;
+  // Read the latest data(auth token, refresh token and expiry)
+  const db = await readFromDb();
+  const data = mostRecent(db.installs);
+  // If no data then continue with the request
+  if (!data) return request;
+  // Data used to calculate the expiry
+  const expiresIn = data.expires_in;
+  const createdDate = data.date;
+  // Used npm library luxon to parse the date and calculate expiry
+  const parsedCreateDate = DateTime.fromISO(createdDate.toString());
+  const expirationDate = parsedCreateDate.plus({ seconds: expiresIn });
+  // Check if expired
+  if (expirationDate < DateTime.now()) {
+    // Create a instance for encryption and decryption
+    const eD = new EncryptDecrypt(process.env[Envars.EncryptionSecret] as string);
+    // Make request to refresh token
+    const { access_token, expires_in, refresh_token, scope, token_type } = await refreshAuthToken(
+      eD.decryptString(data.refresh_token),
+    );
+    // Update the acess and refresh token with the newly fetched access and refresh token
+    // along with the expiry and other required info
+    await updateDb(data, {
+      ...data,
+      access_token: eD.encryptString(access_token),
+      expires_in,
+      refresh_token: eD.encryptString(refresh_token),
+      token_type,
+      scope,
+      date: new Date(),
+    });
   }
   return request;
 }

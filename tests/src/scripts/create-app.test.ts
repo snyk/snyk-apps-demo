9017c51e12d9733856f5ee9931ee2bb242557ef0
Love Bhardwaj <love.bhardwaj@snyk.io>
2021-10-04 19:37:16 +0530

    feat: add redirect uris as option to create script

---
+++ tests/src/scripts/create-app.test.ts
@@ -0,0 +1,47 @@
+import { run } from '../../helper';
+
+/**
+ * Test for create app script
+ */
+describe('Create App', () => {
+  /**
+   * Input validation tests
+   */
+  describe('cli test: input validation', () => {
+    it('should throw error for auth token not provided', () => {
+      expect(() => {
+        run('');
+      }).toThrow('Missing required arguments: authToken');
+    });
+
+    it('should throw error for org id not provided', () => {
+      expect(() => {
+        run('--auth-token=test');
+      }).toThrow('Missing required arguments: orgId');
+    });
+
+    it('should throw error for scopes not providied', () => {
+      expect(() => {
+        run('--auth-token=test --org-id=test');
+      }).toThrow('Missing required arguments: scopes');
+    });
+
+    it('should throw error for name not providied', () => {
+      expect(() => {
+        run('--auth-token=test --org-id=test --scopes=apps:beta');
+      }).toThrow('Missing required argument: name');
+    });
+
+    it('should not throw error when all required args provided', () => {
+      expect(() => {
+        run('--auth-token=test --org-id=test --scopes=apps:beta --name=test');
+      }).not.toThrow('Missing required argument');
+    });
+
+    it('should not throw error when redirectUris also provided', () => {
+      expect(() => {
+        run('--auth-token=test --org-id=test --redirect-uris=http://test.com --scopes=apps:beta --name=test');
+      }).not.toThrow();
+    });
+  });
+});

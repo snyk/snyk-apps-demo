34502617446755513b05f5443d3d07f4be4569c8
Love Bhardwaj <love.bhardwaj@snyk.io>
2022-01-28 00:21:56 +0530

    test: update failing tests

--- tests/src/scripts/create-app.test.ts
+++ tests/src/scripts/create-app.test.ts
@@ -11,37 +11,49 @@ describe('Create App', () => {
     it('should throw error for auth token not provided', () => {
       expect(() => {
         run('');
-      }).toThrow('Missing required arguments: authToken');
+      }).toThrow();
     });
 
     it('should throw error for org id not provided', () => {
-      expect(() => {
-        run('--auth-token=test');
-      }).toThrow('Missing required arguments: orgId');
+      const result = run('--auth-token=test');
+      expect(result).toContain(
+        'Arguments --auth-token, --org-id, --redirect-uris, --scopes, --name, are all required if using the command without the --file argument',
+      );
     });
 
     it('should throw error for scopes not providied', () => {
-      expect(() => {
-        run('--auth-token=test --org-id=test');
-      }).toThrow('Missing required arguments: scopes');
+      const result = run('--auth-token=test --org-id=test');
+      expect(result).toContain(
+        'Arguments --auth-token, --org-id, --redirect-uris, --scopes, --name, are all required if using the command without the --file argument',
+      );
     });
 
     it('should throw error for name not providied', () => {
-      expect(() => {
-        run('--auth-token=test --org-id=test --scopes=apps:beta');
-      }).toThrow('Missing required argument: name');
+      const result = run('--auth-token=test --org-id=test --scopes=apps:beta');
+      expect(result).toContain(
+        'Arguments --auth-token, --org-id, --redirect-uris, --scopes, --name, are all required if using the command without the --file argument',
+      );
     });
 
     it('should not throw error when all required args provided', () => {
-      expect(() => {
-        run('--auth-token=test --org-id=test --scopes=apps:beta --name=test');
-      }).not.toThrow('Missing required argument');
+      const result = run('--auth-token=test --org-id=test --scopes=apps:beta --name=test');
+      expect(result).toContain(
+        'Arguments --auth-token, --org-id, --redirect-uris, --scopes, --name, are all required if using the command without the --file argument',
+      );
     });
 
-    it('should not throw error when redirectUris also provided', () => {
-      expect(() => {
-        run('--auth-token=test --org-id=test --redirect-uris=http://test.com --scopes=apps:beta --name=test');
-      }).not.toThrow();
+    it('should print error to console for token not valid', () => {
+      const result = run(
+        '--auth-token=test --org-id=test --redirect-uris=http://test.com --scopes=apps:beta --name=test',
+      );
+      expect(result).toContain('Error creating app');
+      expect(result).toContain('Unauthorized');
+      expect(result).toContain('401');
+    });
+
+    it('should print error to console when file not found', () => {
+      const result = run('--auth-token=test --file=./something/snyk-app.yaml');
+      expect(result).toContain('ENOENT: no such file or directory');
     });
   });
 });

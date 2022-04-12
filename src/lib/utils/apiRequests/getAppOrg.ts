61ac558483d45e04fcac963d930e7c2e418ad27b
Darrell Mozingo <darrell@mozingo.net>
2022-04-12 15:51:46 -0400

    feat: use /v1/orgs endpoint for now
    Until /rest/orgs supports indirect org membership, for when a Snyk App
    is installed at a group level.

--- src/lib/utils/apiRequests/getAppOrg.ts
+++ src/lib/utils/apiRequests/getAppOrg.ts
@@ -8,6 +8,11 @@ interface RestApiOrg {
   };
 }
 
+interface V1ApiOrg {
+  id: string;
+  name: string;
+}
+
 /**
  * Function to get app's accessible Snyk Org IDs, which are used in other API requests
  * @param {String} tokenType token type which is normally going to be bearer
@@ -19,14 +24,16 @@ export async function getAppOrgs(tokenType: string, accessToken: string): Promis
     const result = await callSnykApi(
       tokenType,
       accessToken,
-      APIVersion.REST,
+      APIVersion.V1,
     )({
       method: 'GET',
       url: `/orgs?version=2022-02-16~experimental`,
     });
 
     return {
-      orgs: result.data.data.map((org: RestApiOrg) => ({ id: org.id, name: org.attributes.name })),
+      // Use v1 until rest endpoint supports indirect org access
+      //orgs: result.data.data.map((org: RestApiOrg) => ({ id: org.id, name: org.attributes.name })),
+      orgs: result.data.orgs.map((org: V1ApiOrg) => ({ id: org.id, name: org.name })),
     };
   } catch (error) {
     console.error('Error fetching org info: ' + error);

81739ecfb98cfc8b1a8e8c4917612096d1dbe7de
Darrell Mozingo <darrell@mozingo.net>
2022-04-12 15:49:13 -0400

    refactor: rename for clarity and remove comment
    We're getting all orgs, and make it explicit the interface is for rest
    api calls.
    
    Also clean up jdocs.

--- src/lib/utils/apiRequests/getAppOrg.ts
+++ src/lib/utils/apiRequests/getAppOrg.ts
@@ -1,7 +1,7 @@
 import { APIVersion, Org } from '../../types';
 import { callSnykApi } from '../api';
 
-interface APIOrg {
+interface RestApiOrg {
   id: string;
   attributes: {
     name: string;
@@ -9,10 +9,9 @@ interface APIOrg {
 }
 
 /**
- * Function to get app's Snyk Org ID i.e the org the app has access to
- * which is used in other API requests
- * @param {String} access_token access token fetched on users behalf
- * @param {String} token_type token type which is normally going to be bearer
+ * Function to get app's accessible Snyk Org IDs, which are used in other API requests
+ * @param {String} tokenType token type which is normally going to be bearer
+ * @param {String} accessToken access token fetched on users behalf
  * @returns Org data or throws and error
  */
 export async function getAppOrgs(tokenType: string, accessToken: string): Promise<{ orgs: Org[] }> {
@@ -26,10 +25,8 @@ export async function getAppOrgs(tokenType: string, accessToken: string): Promis
       url: `/orgs?version=2022-02-16~experimental`,
     });
 
-    // Fetch the first org for demo purposes
-    const orgs = result.data.data;
     return {
-      orgs: orgs.map((org: APIOrg) => ({ id: org.id, name: org.attributes.name })),
+      orgs: result.data.data.map((org: RestApiOrg) => ({ id: org.id, name: org.attributes.name })),
     };
   } catch (error) {
     console.error('Error fetching org info: ' + error);
